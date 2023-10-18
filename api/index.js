c5859e0c4b47a715eae333f2899780544d22f86c
alihamzarao6 <alihamzarao6@email.com>
2023-10-19 00:46:46 +0500

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,9 +1,32 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+
+const connectToMongoDB = async () => {
+  try {
+    // Options to avoid deprecation warnings. You can customize these based on your needs.
+    const options = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    };
+
+    // Connect to MongoDB using Mongoose
+    await mongoose.connect(process.env.MONGO_URI, options);
+
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+  }
+};
+
+connectToMongoDB();
 
 let port = 3000;
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
-})
+  console.log(`Server is running on port: ${port}`);
+});
