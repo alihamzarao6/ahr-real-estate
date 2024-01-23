acf44b73a9611bf0be8b4a70f682761f16853062
Ali Rao <alihamzarao6@gmail.com>
2024-01-23 23:05:02 +0500

    create the ListingItem component and show listings

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import ListingItem from "../components/ListingItem";
 
 const Search = () => {
     const navigate = useNavigate();
-    
+
     const [sidebardata, setSidebardata] = useState({
         searchTerm: '',
         type: 'all',
@@ -13,7 +14,7 @@ const Search = () => {
         sort: 'created_at',
         order: 'desc',
     });
-    
+
     const [loading, setLoading] = useState(false);
     const [listings, setListings] = useState([]);
     console.log(listings);
@@ -222,10 +223,26 @@ const Search = () => {
             {/* left side div Ends */}
 
             {/* Right side div starts */}
-            <div>
+            <div className="flex-1">
                 <h1 className="text-3xl font-semibold border-b p-3 text-slate-700 mt-5">Listing Results:</h1>
+
+                <div className="p-7 flex flex-wrap gap-4">
+                    {!loading && listings.length === 0 && (
+                        <p className="text-xl text-slate-700">No listing found!</p>
+                    )}
+
+                    {loading && (
+                        <p className="text-xl text-slate-700 text-center w-full">Loading...</p>
+                    )}
+
+                    {!loading && listings && listings.map((listing) => (
+                        <ListingItem key={listing._id} listing={listing} />
+                    ))}
+                    
+                </div>
             </div>
             {/* Right side div Ends */}
+
         </div>
     );
 };
